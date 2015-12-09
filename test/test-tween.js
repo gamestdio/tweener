9488de01116fb73c6600c77da4b33e04e678ab08
mdtarso <maurodetarso@gmail.com>
2015-12-09 18:54:56 +0000

    testing

--- test/test-tween.js
+++ test/test-tween.js
@@ -3,6 +3,12 @@
 import {assert} from 'chai';
 import Tween from '../source/Tween';
 
+function update(tween, times = 5, delta = 1) {
+  for (var i = 0; i < times; i++) {
+    tween.update(delta);
+  }
+}
+
 describe('Tween', () => {
   describe('#constructor()', () => {
     it('Should create a valid Tween', () => {
@@ -26,15 +32,14 @@ describe('Tween', () => {
     });
   });
   describe('#to()', () => {
-    it('Should set the target for further params', () => {
-      var objA = {x:0, y:0};
-      var objB = {x:0, y:0};
-      var tween = new Tween(objA);
-      assert.equal(tween._ref, objA);
-      tween.add(objB);
-      assert.equal(tween._ref, objA);
-      assert.equal(tween._target, objB);
-      assert.equal(tween._lastState, objB);
+    it('Should enqueue the desirable action', () => {
+      var obj = {x:0, y:0};
+      var tween = new Tween(obj);
+      tween.to({x:50}, 3);
+      assert.equal(tween._length, 1);
+      assert.equal(tween._queue.length, 1);
+      assert.equal(tween._queue[0].fr, {x:0});
+      assert.equal(tween._queue[0].to, {x:50});
     });
   });
 });
