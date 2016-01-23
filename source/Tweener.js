f437e2a3f1a833bedee40ccd3eabd17dffa3399f
mdtarso <maurodetarso@gmail.com>
2016-01-23 13:27:20 +0000

    documentation

--- source/Tweener.js
+++ source/Tweener.js
@@ -1,42 +1,71 @@
 import Tween from './Tween';
-
+/**
+ * Tweener creates and manages Tween instances.
+ */
 export default class Tweener {
+  /**
+   * Creates an instance of Tweener.
+   * @param {Number} [autoUpdateRate=0] - Auto-update step. If 0, it should be updated manually.
+   */
   constructor(autoUpdateRate = 0) {
-    this.debug = false;
+    /**
+     * Array of current Tween instances.
+     * @type {Array<Tween>}
+     */
     this.tweens = [];
     this._interval = null;
     this.setAutoUpdateRate(autoUpdateRate);
   }
 
+  /**
+   * Destroys the instance.
+   */
   dispose() {
     clearInterval(this._interval);
     this._interval = null;
     this.tweens = null;
   }
 
-  setAutoUpdateRate(value) {
+  /**
+   * Sets the auto-update rate, the default time interval between updates.
+   * @param {Number} [rate=0] - Auto-update step. If 0, it should be updated manually.
+   */
+  setAutoUpdateRate(rate = 0) {
     clearInterval(this._interval);
     this._interval = null;
 
-    if (value > 0) {
-      this._time = this.getTime();
-      this._interval = setInterval(this._autoUpdate.bind(this), value*1000);
+    if (rate > 0) {
+      this._time = this._getTime();
+      this._interval = setInterval(this._autoUpdate.bind(this), rate*1000);
     }
   }
 
   _autoUpdate() {
-    var t = this.getTime();
+    var t = this._getTime();
     var d = t - this._time;
     this._time = t;
     this.update(d);
   }
 
+  _getTime() {
+    return new Date().getTime()/1000;
+  }
+
+  /**
+   * Creates and returns a new Tween instance based on given object.
+   * @param {Object} obj - The object which will be updated.
+   * @returns {Tween} The Tween instance created.
+   */
   add(obj) {
     var tween = new Tween(obj);
     this.tweens.push(tween);
     return tween;
   }
 
+  /**
+   * Removes all Tween instances based on given object.
+   * @param {Object} obj - The object used as reference.
+   */
   remove(obj) {
     var i = this.tweens.length;
     while (i--) {
@@ -47,7 +76,11 @@ export default class Tweener {
     }
   }
 
-  update(delta) {
+  /**
+   * Updates all Tweens. Should be called manually if auto-update rate is 0.
+   * @param {Number} [delta=0] - The desired time step for update.
+   */
+  update(delta = 0) {
     var i = this.tweens.length;
     while (i--) {
       var t = this.tweens[i];
@@ -55,9 +88,5 @@ export default class Tweener {
         this.tweens.splice(i, 1);
       }
     }
-  }
-
-  getTime() {
-    return new Date().getTime()/1000;
   }
 }
